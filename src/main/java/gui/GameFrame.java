0ebfe9723a2c9637a30f4d69bb2271988121169d
TheAlexmen11 <alexdcancho@gmail.com>
2025-05-22 00:00:48 -0500

    avanzando con el juego

--- src/main/java/gui/GameFrame.java
+++ src/main/java/gui/GameFrame.java
@@ -5,23 +5,33 @@ import static gui.GamePanel.WIDTH;
 import java.awt.Dimension;
 import javax.swing.JFrame;
 
-public class GameFrame extends javax.swing.JFrame {
-    
+public class GameFrame extends javax.swing.JFrame  {
+
     static final int WIDTH = 515;
     static final int HEIGHT = 535;
-    
+    GameFondo fondo;
+    GamePanel panel;
+
     public GameFrame() {
-        
-        GameFondo panel = new GameFondo();
+
+        panel = new GamePanel();
         this.add(panel);
+        panel.setBounds(0, 0, 500, 500);
+ panel.setOpaque(false);
+        fondo = new GameFondo();
+        this.add(fondo);
+        fondo.setBounds(0, 0, 500, 500);
+       
+
+        //.setOpaque(false);
         this.setTitle("snake");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT+5));
         this.setResizable(false);
         this.pack();
         this.setVisible(true);
-        this.setLocationRelativeTo(null);
-        
+        //this.setLocationRelativeTo(null);
+
         initComponents();
 
     }
@@ -31,6 +41,11 @@ public class GameFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                formKeyPressed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -45,6 +60,10 @@ public class GameFrame extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void formKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyPressed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_formKeyPressed
 
     public static void main(String args[]) {
 
--- src/main/java/gui/GamePanel.java
+++ src/main/java/gui/GamePanel.java
@@ -2,25 +2,151 @@ package gui;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import static java.awt.image.ImageObserver.HEIGHT;
 import static java.awt.image.ImageObserver.WIDTH;
+import java.util.Random;
+import javax.swing.Timer;
 
+public class GamePanel extends javax.swing.JPanel implements ActionListener {
 
-public class GamePanel extends javax.swing.JPanel {
     static final int WIDTH = 500;
     static final int HEIGHT = 500;
+    static final int UNIT_SIZE = 20;
+    static final int NUMBER_OF_UNITS = (WIDTH * HEIGHT) / (UNIT_SIZE * UNIT_SIZE);
+
+    // hold x and y coordinates for body parts of the snake
+    final int x[] = new int[NUMBER_OF_UNITS];
+    final int y[] = new int[NUMBER_OF_UNITS];
+
+    // initial length of the snake
+    int length = 5;
+    int foodEaten;
+    int foodX;
+    int foodY;
+    char direction = 'D';
+    boolean running = false;
+    Random random;
+    Timer timer;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         this.setBackground(Color.DARK_GRAY);
         this.setFocusable(true);
+        random = new Random();
+        play();
         initComponents();
     }
 
+    @Override
+    public void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+        draw(graphics);
+    }
+
+    public void play() {
+        addFood();
+        running = true;
+
+        timer = new Timer(80, (ActionListener) this);
+        timer.start();
+    }
+
+    public void draw(Graphics graphics) {
+
+        if (running) {
+            graphics.setColor(new Color(210, 115, 90));
+            graphics.fillOval(foodX, foodY, UNIT_SIZE, UNIT_SIZE);
+
+            graphics.setColor(new Color(210, 115, 90));
+            graphics.setColor(Color.white);
+            graphics.fillOval(x[0], y[0], UNIT_SIZE, UNIT_SIZE);
+            for (int i = 1; i < length; i++) {
+                graphics.setColor(new Color(40, 200, 150));
+                graphics.fillOval(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+
+            //graphics.setColor(Color.white);
+            //graphics.setFont(new Font("Sans serif", Font.ROMAN_BASELINE, 25));
+            //FontMetrics metrics = getFontMetrics(graphics.getFont());
+            //graphics.drawString("Score: " + foodEaten, (WIDTH - metrics.stringWidth("Score: " + foodEaten)) / 2, graphics.getFont().getSize());
+        } else {
+            gameOver(graphics);
+        }
+    }
+
+    public void actionPerformed(ActionEvent arg0) {
+        if (running) {
+            move();
+            checkFood();
+            //checkHit();
+        }
+        repaint();
+    }
+
+    public void checkFood() {
+        if (x[0] == foodX && y[0] == foodY) {
+            length++;
+            foodEaten++;
+            addFood();
+        }
+    }
+
+    public void move() {
+        for (int i = length; i > 0; i--) {
+            // shift the snake one unit to the desired direction to create a move
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
+
+        if (direction == 'L') {
+            System.out.println("L");
+            x[0] = x[0] - UNIT_SIZE;
+        } else if (direction == 'R') {
+            System.out.println("R");
+            x[0] = x[0] + UNIT_SIZE;
+        } else if (direction == 'U') {
+            y[0] = y[0] - UNIT_SIZE;
+        } else {
+            y[0] = y[0] + UNIT_SIZE;
+        }
+        x[0] = Math.floorMod(x[0], WIDTH);
+        y[0] = Math.floorMod(y[0], HEIGHT);
+    }
+
+    public void addFood() {
+        foodX = random.nextInt((int) (WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+        foodY = random.nextInt((int) (HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
+    }
+
+    public void gameOver(Graphics graphics) {
+        graphics.setColor(Color.red);
+        graphics.setFont(new Font("Sans serif", Font.ROMAN_BASELINE, 50));
+        FontMetrics metrics = getFontMetrics(graphics.getFont());
+        graphics.drawString("Game Over", (WIDTH - metrics.stringWidth("Game Over")) / 2, HEIGHT / 2);
+
+        graphics.setColor(Color.white);
+        graphics.setFont(new Font("Sans serif", Font.ROMAN_BASELINE, 25));
+        metrics = getFontMetrics(graphics.getFont());
+        graphics.drawString("Score: " + foodEaten, (WIDTH - metrics.stringWidth("Score: " + foodEaten)) / 2, graphics.getFont().getSize());
+
+    }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+
+        addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                formKeyPressed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -33,6 +159,35 @@ public class GamePanel extends javax.swing.JPanel {
             .addGap(0, 473, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void formKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyPressed
+        switch (evt.getKeyCode()) {
+            case KeyEvent.VK_LEFT:
+                if (direction != 'R') {
+                    direction = 'L';
+                }
+                break;
+
+            case KeyEvent.VK_RIGHT:
+                if (direction != 'L') {
+                    direction = 'R';
+                }
+                break;
+
+            case KeyEvent.VK_UP:
+                if (direction != 'D') {
+                    direction = 'U';
+                }
+                break;
+
+            case KeyEvent.VK_DOWN:
+                if (direction != 'U') {
+                    direction = 'D';
+                }
+                break;
+        }
+
+    }//GEN-LAST:event_formKeyPressed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
